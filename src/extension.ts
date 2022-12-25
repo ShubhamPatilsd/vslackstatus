6a678eaa87ed395c243208d0de6237e26aaf5d4b
Shubham Patil <shubhampatilsd@gmail.com>
2022-12-25 14:16:58 -0800

    got editing to work

--- src/extension.ts
+++ src/extension.ts
@@ -5,15 +5,11 @@ import * as path from "path";
 
 import { App } from "@slack/bolt";
 
-// const app = new App({
-//   signingSecret: process.env.SLACK_SIGNING_SECRET,
-//   token: process.env.SLACK_BOT_TOKEN,
-// });
-
 const app = new App({
-  signingSecret: "3c6568e6db44d9babe5c8b0c759be0d7",
-  token:
-    "xoxp-2210535565-2319185552498-4566684101556-5cd3c328c2cba97461054bf1fc929512",
+  signingSecret: vscode.workspace
+    .getConfiguration("vslack")
+    .get("signingSecret"),
+  token: vscode.workspace.getConfiguration("vslack").get("userToken"),
 });
 
 // this method is called when your extension is activated
@@ -21,15 +17,31 @@ const app = new App({
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "vslack" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
+  vscode.window.showInformationMessage(vscode.workspace.name + "hi");
+
+  const initialStatus = vscode.window.activeTextEditor?.document.fileName
+    ? {
+        profile: JSON.stringify({
+          status_text: `Editing ${
+            path.parse(vscode.window.activeTextEditor.document.fileName || "")
+              .base
+          } in ${vscode.workspace.name}`,
+          status_emoji: ":vsc:",
+          status_expiration: 0,
+        }),
+      }
+    : {
+        profile: JSON.stringify({
+          status_text: `Idle in ${vscode.workspace.name}`,
+          status_emoji: ":vsc:",
+          status_expiration: 0,
+        }),
+      };
+
+  app.client.users.profile.set(initialStatus);
+
   let disposable = vscode.window.onDidChangeActiveTextEditor((editor) => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-
     if (editor?.document.fileName) {
       app.client.users.profile.set({
         profile: JSON.stringify({
@@ -50,7 +62,9 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {
   app.client.users.profile.set({
     profile: JSON.stringify({
-      status_text: ``,
+      status_text: "",
+      status_emoji: "",
+      status_expiration: 0,
     }),
   });
 }
